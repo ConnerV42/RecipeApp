960d6d4754cba239cee9b630c4ec08fcc4d3abd0
connerv42 <connerverret42@gmail.com>
2018-12-20 18:33:04 -0800

    Finish recipe component, add styles

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -7,7 +7,12 @@ class RecipeApp extends Component {
   render() {
     return (
       <div className="App">
-        <Recipe />
+        <Recipe
+          title="pasta"
+          ingredients={['flour', 'water']}
+          instructions="Mix Ingredients"
+          img="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/homemade-spaghetti-sauce-horizontal-1530890913.jpg?crop=1xw:1xh;center,top&resize=768:*"
+        />
       </div>
     );
   }
--- src/recipe.js
+++ src/recipe.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import './recipe.css';
 
 class Recipe extends Component {
     render() {
+        // Destructuring this object (title is valid as long as this.props.title exists)
+        const { title, img, instructions } = this.props;
+        const ingredients = this.props.ingredients.map((ing, index) => (
+            <li key={index}>{ing}</li> // one line of code means you don't need to return from map
+        ));
         return (
-            <div>Recipe</div>
+            <div className="recipe-card">
+                <div className="recipe-card-img">
+                    <img src={img} alt={title} />
+                </div>
+                <div className="recipe-card-content">
+                    <h3 className="recipe-title">{title}</h3>
+                    <h4>Ingredients: </h4>
+                    <ul>
+                        {ingredients}
+                    </ul>
+                    <h4>Instructions</h4>
+                    <p>
+                        {instructions}
+                    </p>
+                </div>
+            </div>
         );
     }
 }
