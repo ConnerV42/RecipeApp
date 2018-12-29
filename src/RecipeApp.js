e31e62fdbeba3217f404d0f1ab163860fb4adbad
connerv42 <connerverret42@gmail.com>
2018-12-28 23:51:31 -0800

    add RecipeInput component (form), add state of app to RecipeApp component

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -2,15 +2,59 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Navbar from './Navbar';
 import Recipe from './recipe';
+import RecipeInput from './RecipeInput';
 import RecipeList from './RecipeList';
 import './RecipeApp.css';
 
 class RecipeApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [
+        {
+          id: 0,
+          title: "Spaghetti",
+          instructions: "Open jar of Spaghetti sauce. Bring to simmer. Boil water. Cook pasta until done. Combine pasta and sauce.",
+          ingredients: ["Pasta", "8 cups of water", "1 can of spaghetti sauce"],
+          img: "/spaghetti.jpg"
+        },
+        {
+          id: 1,
+          title: "Milkshake",
+          instructions: "Combine ice cream and milk. Blend until creamy.",
+          ingredients: ["2 scoops of ice cream", "8 ounces of milk"],
+          img: "/milkshake.jpg"
+        },
+        {
+          id: 2,
+          title: "Avocado Toast",
+          instructions: "Toast bread. Slice avocado and spread on bread. Add salt, oil and pepper to taste.",
+          ingredients: ["2 slices of break", "1 avocado", "1 tablespoon of olive oil", "1 pinch of salt", "pepper"],
+          img: "/avocado_toast.jpg"
+        }
+      ],
+      nextRecipeId: 3,
+    }
+
+    this.handleSave = this.handleSave.bind(this);
+  }
+
+  handleSave(recipe) {
+    this.setState((prevState, props) => {
+      const newRecipe = { ...recipe, id: prevState.nextRecipeId };
+      return {
+        nextRecipeId: prevState.nextRecipeId,
+        recipes: [...this.state.recipes, newRecipe]
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
-        <RecipeList />
+        <RecipeInput onSave={this.handleSave} />
+        <RecipeList recipes={this.state.recipes} />
       </div>
     );
   }
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -4,28 +4,6 @@ import Recipe from './recipe';
 import PropTypes from 'prop-types';
 
 class RecipeList extends Component {
-    static defaultProps = {
-        recipes: [
-            {
-                title: "Spaghetti",
-                instructions: "Open jar of Spaghetti sauce. Bring to simmer. Boil water. Cook pasta until done. Combine pasta and sauce.",
-                ingredients: ["Pasta", "8 cups of water", "1 can of spaghetti sauce"],
-                img: "/spaghetti.jpg"
-            },
-            {
-                title: "Milkshake",
-                instructions: "Combine ice cream and milk. Blend until creamy.",
-                ingredients: ["2 scoops of ice cream", "8 ounces of milk"],
-                img: "/milkshake.jpg"
-            },
-            {
-                title: "Avocado Toast",
-                instructions: "Toast bread. Slice avocado and spread on bread. Add salt, oil and pepper to taste.",
-                ingredients: ["2 slices of break", "1 avocado", "1 tablespoon of olive oil", "1 pinch of salt", "pepper"],
-                img: "/avocado_toast.jpg"
-            }
-        ]
-    }
     static propTypes = {
         recipes: PropTypes.arrayOf(PropTypes.object).isRequired
     }
